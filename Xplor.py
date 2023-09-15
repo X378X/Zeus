bc257e5c93ac5fb4ad790e4cc8d30aeade003d9c
MUHAMAD RIZKI <119448322+X378X@users.noreply.github.com>
2023-09-16 01:47:08 +0700

    Update Xplor.py

--- Xplor.py
+++ Xplor.py
@@ -809,6 +809,8 @@ def crack(idf,pwv):
 	
 #----------[ SYSTEM-CONTROL ]----------#	
 if __name__=='__main__':
+	try:os.system('git pull')
+	except:pass
 	try:os.mkdir('OK')
 	except:pass
 	try:os.mkdir('CP')
