d9d1ff5cec32188ebfccfd237b2790a813eb8a2f
MUHAMAD RIZKI <119448322+X378X@users.noreply.github.com>
2023-10-26 01:27:08 +0700

    Update Xplor.py

--- Xplor.py
+++ Xplor.py
@@ -56,7 +56,7 @@ for xd in range(10000):
     j=random.randrange(4200,4900)
     k=random.randrange(40,150)
     l='Mobile Safari/537.36'
-    uaku2=(f'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Mobile Safari/537.36')
+    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
     ugen.append(uaku2)
 def uaku():
 	try:
