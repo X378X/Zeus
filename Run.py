08fb959bd6b33e1fb65ebebd364e55d154fb3bcb
MUHAMAD RIZKI <119448322+X378X@users.noreply.github.com>
2023-12-06 08:48:06 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -533,7 +533,7 @@ class Crack:
 							
 	###----------[ METODE API ]---------- ###
 	def metode_api(self,email,pwx):
-		prog.update(des,description=f" {H2}°{P2} 🕐 {H2}RETAS°{P2} {str(self.loop)}/{len(tampung)} OK : {H2}{len(self.ok)}{P2} CP : {K2}{len(self.cp)}{P2}")
+		prog.update(des,description=f" {H2}°{P2}{H2}RETAS°{P2} {str(self.loop)}/{len(tampung)} OK : {H2}{len(self.ok)}{P2} CP : {K2}{len(self.cp)}{P2}")
 		prog.advance(des)
 		try:
 			for pw in pwx:
